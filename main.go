b1e626dfbfa0bfe3bb11e5c0f551bcad56f1428e
Auronen <HThiele@seznam.cz>
2022-10-29 12:55:11 +0200

    add item example

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"zen_test/parsers"
 
@@ -12,11 +13,18 @@ func main() {
 	file, _ := os.Open("WORLD.ZEN")
 	zen := parsers.NewBsZen()
 	zen.Read(kaitai.NewStream(file), nil, zen)
-	wplist := zen.GetWaypointList()
 
+	// Prints all waypoints to standard output
+	wplist := zen.GetWaypointList()
 	f := bufio.NewWriter(os.Stdout)
 	defer f.Flush()
 	for _, wp := range wplist {
 		f.WriteString(wp + "\n")
+	}
+
+	// Prints all items to standard output
+	itemlist := zen.GetItemList()
+	for key, amount := range itemlist {
+		f.WriteString(fmt.Sprintf("%s: %d\n", key, amount))
 	}
 }
--- parsers/bs_extras.go
+++ parsers/bs_extras.go
@@ -1,18 +1,11 @@
 package parsers
 
 import (
-	"fmt"
-	"os"
 	"strings"
 )
 
 func (zen *BsZen) GetWaypointList() (list []string) {
 	list = make([]string, 0, 20)
-	fmt.Fprintf(os.Stdout, "zen: %v\n", zen)
-	fmt.Fprintf(os.Stdout, "OCWorld: %v\n", zen.OCWorld)
-	fmt.Fprintf(os.Stdout, "OCWorld: %v\n", zen.OCWorld)
-	fmt.Fprintf(os.Stdout, "len(zen.OCWorld.Properties: %v\n", len(zen.OCWorld.Properties))
-
 	for i := 0; i < len(zen.OCWorld.Properties); i++ {
 		if zen.OCWorld.Properties[i].Type == 1 { // string type 0x1
 			objectID := zen.OCWorld.Properties[i].PropBody.(*BsZen_TypeString).ObjectId
@@ -25,4 +18,17 @@ func (zen *BsZen) GetWaypointList() (list []string) {
 		}
 	}
 	return list
+}
+
+func (zen *BsZen) GetItemList() map[string]int {
+	items := make(map[string]int, 20)
+	for i := 0; i < len(zen.OCWorld.Properties); i++ {
+		if zen.OCWorld.Properties[i].Type == 1 { // string type 0x1
+			objectID := zen.OCWorld.Properties[i].PropBody.(*BsZen_TypeString).ObjectId
+			if strings.Contains(objectID, "oCItem:zCVob") {
+				items[zen.OCWorld.Properties[i+6].PropBody.(*BsZen_TypeString).ObjectId] += 1
+			}
+		}
+	}
+	return items
 }
